868b480e2eebeba591b39b5551f62a8f37bbca8f
Kiran <sangqt.inb@gmail.com>
2024-12-31 22:33:20 +0900

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ChirpController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store', 'udpate', 'destroy'])
+    ->middleware(['auth', 'verified']);
